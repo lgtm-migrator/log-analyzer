9a54c43164b9f2c4f8b5390f9684e9f03e0af83f
Antonio Molner Domenech <antoniomdk@hotmail.es>
2019-05-02 17:37:07 +0200

    Remove unused arguments

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -13,15 +13,15 @@ from cassandra.cluster import Cluster
 APACHE_ACCESS_LOG_PATTERN = r'^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+) (\S+)" (\d{3}) (\d+)'
 
 """
-            ip_address=match.group(1),
-            client_identd=match.group(2),
-            user_id=match.group(3),
-            date_time=match.group(4),
-            method=match.group(5),
-            endpoint=match.group(6),
-            protocol=match.group(7),
-            response_code=int(match.group(8)),
-            content_size=int(match.group(9))]
+    ip_address=match.group(1),
+    client_identd=match.group(2),
+    user_id=match.group(3),
+    date_time=match.group(4),
+    method=match.group(5),
+    endpoint=match.group(6),
+    protocol=match.group(7),
+    response_code=int(match.group(8)),
+    content_size=int(match.group(9))]
 """
 
 def parse_log_line(line):
@@ -48,29 +48,30 @@ def send_data(rdd):
 
 
 def process(partition):
-    session = connect_to_cassandra(['localhost'])
+    session = connect_to_cassandra()
     for row in partition:
         insert_into_db(row, session)
     # TODO: send data to dashboard
 
 
 def insert_into_db(row, session):
-    #row = row.asDict()
     query = """
-	INSERT INTO logs (ip_address, client_identd, user_id, date_time, method, endpoint, protocol, response_code, content_size)
-	VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        INSERT INTO logs (
+        ip_address, client_identd, user_id,
+        date_time, method, endpoint, protocol,
+        response_code, content_size)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
-    print(query)
     session.execute(query, row)
 
 
 def connect_to_cassandra(cluster_ips):
-    cluster = Cluster(cluster_ips)
+    cluster = Cluster()
     session = cluster.connect('logs_keyspace')
     return session
 
 
-def main(interval, kafka_broker_list, topic, cassandra_server):
+def main(interval, topic):
     sc = SparkContext("local[*]", "LogAnalyzer")
     ssc = StreamingContext(sc, interval)
     kafka_stream = KafkaUtils.createDirectStream(
@@ -86,9 +87,5 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('interval', type=int, help='In seconds')
     parser.add_argument('topic', type=str, help='Kafka topic')
-    parser.add_argument(
-        'cassandra_server', type=str, help='Cassandra server ip')
-    parser.add_argument(
-        'kafka_broker_list', type=str, help='Kafka broker list', nargs='+')
     args = vars(parser.parse_args())
     main(**args)
