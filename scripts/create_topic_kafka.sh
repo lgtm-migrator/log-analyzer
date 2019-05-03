ce74cbb17dcb02b3704cb3708e846cfb2a5a3bf6
Antonio Molner Domenech <antoniomdk@hotmail.es>
2019-05-03 20:23:48 +0200

    Fix topic creator script

--- scripts/create_topic_kafka.sh
+++ scripts/create_topic_kafka.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-bin/kafka-topics.sh --zookeeper logger.descifrandolinux.com:2181 --create --replication-factor 1 --partitions 1 --topic log_topic
+$KAFKA_DIR/bin/kafka-topics.sh \
+  --zookeeper logger.descifrandolinux.com:2181 \
+  --create --replication-factor 1 --partitions 1 \
+  --topic $1
